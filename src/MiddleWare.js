b2881a7fc1242f93c9d16f71c72005231aa7d0e9
Hardik Pithadiya <pithadiyahardik646@gmail.com>
2022-02-25 21:50:00 +0530

    Solved bug for admin access

--- src/MiddleWare.js
+++ src/MiddleWare.js
@@ -3,7 +3,6 @@ const models = require("./Models");
 const adminAccess=async (req,res,next) =>{
     let email=req.body.email;
     let password=req.body.admin;
-    console.log(email+" "+password);
     let user=await models.UserModel.find({email:email,password:password,role:"admin"});
     if(user.length!=0)
     {
