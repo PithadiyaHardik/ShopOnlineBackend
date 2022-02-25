7a8f317d0640be2cf75b677026bd4174aa648ac5
Hardik Pithadiya <pithadiyahardik646@gmail.com>
2022-02-25 22:04:10 +0530

    Updated middleware

--- src/MiddleWare.js
+++ src/MiddleWare.js
@@ -3,6 +3,7 @@ const models = require("./Models");
 const adminAccess=async (req,res,next) =>{
     let email=req.body.email;
     let password=req.body.admin;
+    console.log(email+" "+password);
     let user=await models.UserModel.find({email:email,password:password,role:"admin"});
     if(user.length!=0)
     {
