9733683a1850a9ff057d3cf5e8d7a8a3ca31e5dc
Hardik Pithadiya <pithadiyahardik646@gmail.com>
2022-02-25 22:05:14 +0530

    Updated middleware

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let PORT=process.env.PORT || 8888
 const app = express();
 app.use(cors({
   origin: 'https://shoponlinefront.herokuapp.com',
-  // origin:"http://localhost:3000"
+  origin:"http://localhost:3000"
 }));
 app.use(express.static(__dirname))
 app.use(express.json());
